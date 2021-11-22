b186d7a4abb608e8cd50c1a7cde7693d47e60ca8
jontzon <52744821+jontzon@users.noreply.github.com>
2021-11-22 16:00:36 +0100

    Update getVmStates.py
    added multischool compatibility, main improvements

--- usr/lib/linuxmuster-linbo-vdi/getVmStates.py
+++ usr/lib/linuxmuster-linbo-vdi/getVmStates.py
@@ -11,7 +11,7 @@ import json
 import time
 import sys
 from datetime import datetime
-from globalValues import node,proxmox,dbprint,checkConnections,timeoutConnectionRequest,getMasterDetails,getFileContent,getCommandOutput,getVDIGroups
+from globalValues import node,getSchoolId,proxmox,dbprint,checkConnections,timeoutConnectionRequest,getMasterDetails,getFileContent,getCommandOutput,getVDIGroups,getSmbstatus
 import re
 
 
@@ -90,7 +90,7 @@ def addUser(vmid, logedIn):
             allallInfos[vmid]["user"] = user
             dbprint(allallInfos[vmid]["user"])
         else:
-             allallInfos[vmid]["user"] = ""
+            allallInfos[vmid]["user"] = ""
 
 
 ######## merges all vm infos to one JSON and returns it with vmid ########
@@ -105,27 +105,27 @@ def mergeInfos(vmid, apiInfos, groupInfos):
 
 
 ####### get Collection of all VMs who are registered at school server #######
-def getVmidRange(vdiGroup):
+def getVmidRange(devicePath,vdiGroup):
     #sftp = ssh.open_sftp()
-    remotePath = "/etc/linuxmuster/sophomorix/default-school/devices.csv"
-    output = getFileContent(remotePath)
-    #output = sftp.open(remotePath)
+    #devicePath = "/etc/linuxmuster/sophomorix/default-school/devices.csv"
+    output = getFileContent(devicePath)
+    #output = sftp.open(devicePath)
     #print("*** Check VM-ID-Range from Group ***")
     vmidRange = []
     for line in output:
         if vdiGroup in line:
-            if "master" not in line:
+            if "master" not in line and line.split(';')[11] is not "":
                 vmid = line.split(';')[11]
                 vmidRange.append(vmid)
     return vmidRange
 
 
 ######## returns dict devicesInfos from devices list  ########
-def getGroupInfos(vmid):
+def getGroupInfos(devicePath, vmid):
     #sftp = ssh.open_sftp()
-    remotePath = "/etc/linuxmuster/sophomorix/default-school/devices.csv"
-    output = getFileContent(remotePath)
-    # output = sftp.open(remotePath)
+    #devicePath = "/etc/linuxmuster/sophomorix/default-school/devices.csv"
+    output = getFileContent(devicePath)
+    # output = sftp.open(devicePath)
     devicesInfos = {}
     for line in output:
         if line.split(';')[11] == vmid:
@@ -138,56 +138,34 @@ def getGroupInfos(vmid):
     return devicesInfos
 
 
-def getGroupInfosMaster(masterHostname):
+def getGroupInfosMaster(devicePath, masterHostname):
     # sftp = ssh.open_sftp()
-    remotePath = "/etc/linuxmuster/sophomorix/default-school/devices.csv"
-    output = getFileContent(remotePath)
-    # output = sftp.open(remotePath)
-    masterInfos = {}
+    output = getFileContent(devicePath)
+    # output = sftp.open(devicePath)
+    vdiGroupInfos = {}
     for line in output:
         if masterHostname in line:
-            masterInfos['room'] = line.split(';')[0]
-            masterInfos['hostname'] = line.split(';')[1]
-            masterInfos['group'] = line.split(';')[2]
-            masterInfos['mac'] = line.split(';')[3]
-            masterInfos['ip'] = line.split(';')[4]
-            masterInfos['pxe'] = line.split(';')[10]
-    return masterInfos
+            vdiGroupInfos['room'] = line.split(';')[0]
+            vdiGroupInfos['hostname'] = line.split(';')[1]
+            vdiGroupInfos['group'] = line.split(';')[2]
+            vdiGroupInfos['mac'] = line.split(';')[3]
+            vdiGroupInfos['ip'] = line.split(';')[4]
+            vdiGroupInfos['pxe'] = line.split(';')[10]
+    return vdiGroupInfos
 
 
-###### returns dict{} logedIn{user : { ip : ip, domain : domain}
-def getLogedinUser():
-    command = "smbstatus | grep users"
-    object = getCommandOutput(command)
-    logedIn = {}
-    for line in object:
-        line = str(line, 'ascii')
-        ip = line.split()[3]
-        user = line.split()[1]
-        domain, user = user.split("\\")
-        # Gegencheck domain and "users":
-        if line.split()[2] == "users":
-                logedIn[user]= {"ip": ip, "domain": domain, "full": r"{}\{}".format(domain,user)}
-    #dbprint("***** Assigned: *****")
-    #dbprint(json.dumps(logedIn, indent=2))
-    return logedIn
-
-
-def getActualImagesize(vdiGroup):
-    #sftp = ssh.open_sftp()
-    #cloopValues = {}
-    remotePath = "/srv/linbo/start.conf." + str(vdiGroup)
-    output = getFileContent(remotePath)
-    #output = sftp.open(remotePath)
+def getActualImagesize(devicePath, vdiGroup):
+    devicePath = "/srv/linbo/start.conf." + str(vdiGroup)
+    output = getFileContent(devicePath)
     cloopline = []
     for line in output:
         if "BaseImage" in line:
             cloopline = line.split(' ')
     cloop = cloopline[2].strip()
 
-    remotePathCloop = ("/srv/linbo/" + str(cloop) + ".info")
-    output2 = getFileContent(remotePathCloop)
-    # output2 = sftp.open(remotePathCloop)
+    devicePathCloop = ("/srv/linbo/" + str(cloop) + ".info")
+    output2 = getFileContent(devicePathCloop)
+    # output2 = sftp.open(devicePathCloop)
     lines = output2.readlines()
     for line in lines:
         linex = line.split("=")
@@ -209,18 +187,24 @@ def mainClones(group = "all", quiet=False):
 
     allallInfos = {}
     for vdiGroup in allGroups:
-####### Get Group Information #########
-        #dbprint("***** Getting information to Clones from Group " + vdiGroup + " *****")
+
+        vdiGroupInfos = getMasterDetails(vdiGroup)
+
+        schoolId = getSchoolId(vdiGroup)
+        devicePath = str
+        if schoolId != "" or schoolId != "default-school":
+            devicePath = "/etc/linuxmuster/sophomorix/" + str(schoolId) + "/" + str(schoolId) + ".devices.csv"
+        else:
+            devicePath = "/etc/linuxmuster/sophomorix/default-school/devices.csv"
 
 ####### Get collected JSON Info File to all VMs from Group #############
         #dbprint("***** ID Range for imagegroup: " + vdiGroup + " *****")
-        idRange = getVmidRange(vdiGroup)
-        #dbprint(idRange)
+        idRange = getVmidRange(devicePath,vdiGroup)
 
 ####### collect API Parameter and Group Infos from each VM, merges them, and collects them in one list #######
         for vmid in idRange:
             apiInfos = {}
-            groupInfos = getGroupInfos(vmid)
+            groupInfos = getGroupInfos(devicePath, vmid)
             try:
                 apiInfos = getApiInfos(node,vmid)
             except Exception as err:
@@ -231,21 +215,22 @@ def mainClones(group = "all", quiet=False):
 
 ####### adds user field if vm is used by an user #######
         # grep nach users !
-        logedIn = getLogedinUser()
+        logedIn = getSmbstatus(schoolId)
+        
+        #print(logedIn)
         if logedIn:
             for user in logedIn:
                 for vmid in idRange:
                     if "buildstate" in allallInfos[vmid]:
                         if logedIn[user]["ip"] == allallInfos[vmid]["ip"]:
                             allallInfos[vmid]["user"] = logedIn[user]["full"]
-                         else:
+                        else:
                             allallInfos[vmid]["user"] = ""
                     else:
                         allallInfos[vmid]["user"] = ""
         else:
             for vmid in idRange:
                 allallInfos[vmid]["user"] = ""
-
 
 
 ####### calculates summary values 'allocated_vms', 'existing_vms', 'building_vms' #######
@@ -262,7 +247,7 @@ def mainClones(group = "all", quiet=False):
         for vmid in allallInfos:
             # calculate existing
             try:
-                proxmox.nodes('hv01').qemu(vmid).status.get()
+                proxmox.nodes(node).qemu(vmid).status.get()
                 existing = existing + 1
                 # calculate building,finished,available,allocated,failed - just from existing vms!
                 try:
@@ -271,16 +256,16 @@ def mainClones(group = "all", quiet=False):
                     elif allallInfos[vmid]['buildstate'] == "finished":
                         if (allallInfos[vmid]['lastConnectionRequestTime'] == ""):
                             if allallInfos[vmid]['user'] == "":
-                                    available = available + 1
+                                available = available + 1
                             elif allallInfos[vmid]["user"] != "":
                                 allocated = allocated + 1
                         elif (allallInfos[vmid]['lastConnectionRequestTime'] != ""):
                             if allallInfos[vmid]['user'] == "" \
-                                and (now - float(allallInfos[vmid]['lastConnectionRequestTime']) > timeoutConnectionRequest):
-                                    available = available + 1
+                                    and (now - float(allallInfos[vmid]['lastConnectionRequestTime']) > timeoutConnectionRequest):
+                                        available = available + 1
                             elif allallInfos[vmid]["user"] != "" \
-                                or (now - float(allallInfos[vmid]['lastConnectionRequestTime']) <= timeoutConnectionRequest):
-                                    allocated = allocated + 1
+                                    or (now - float(allallInfos[vmid]['lastConnectionRequestTime']) <= timeoutConnectionRequest):
+                                        allocated = allocated + 1
                     elif allallInfos[vmid]['buildstate'] == "failed":
                         failed = failed + 1
                 except Exception as err:
@@ -304,16 +289,18 @@ def mainClones(group = "all", quiet=False):
             allallGroupInfos[vdiGroup]['summary']  = summary
         else:
             allallInfos['summary'] = summary
-
+    
 ####### prints the whole JSON with all information #######
 
     if group == "all":
-        dbprint(json.dumps(allallGroupInfos, indent=2))
+        if not quiet:
+            dbprint(json.dumps(allallGroupInfos, indent=2))
         return allallGroupInfos
     else:
-        dbprint(json.dumps(allallInfos, indent=2))
+        if not quiet:
+            dbprint(json.dumps(allallInfos, indent=2))
         return allallInfos
-
+    
 
 ####### MASTER: #######
 def mainMaster(group="all", quiet=False):
@@ -323,25 +310,36 @@ def mainMaster(group="all", quiet=False):
     if group == "all":
         allGroups = getVDIGroups()
         allGroupInfos = {}
+        #print("=======AllGroups:")
+        #print(allGroups)
     else:
         allGroups.append(group)
 
     groupInfos = {}
     for vdiGroup in allGroups:
+
+        vdiGroupInfos = getMasterDetails(vdiGroup)
+        schoolId = getSchoolId(vdiGroup)
+        devicePath = str
+        if schoolId != "" or schoolId != "default-school":
+            devicePath = "/etc/linuxmuster/sophomorix/" + str(schoolId) + "/" + str(schoolId) + ".devices.csv"
+        else:
+            devicePath = "/etc/linuxmuster/sophomorix/default-school/devices.csv"
+
+
         ####### Get collected JSON Info File to all VMs from Group #############
         #dbprint("*** ID Range for imagegroup: " + vdiGroup + " ***")
-        masterInfos = getMasterDetails(vdiGroup)
-        masterVmids = masterInfos['vmids'].split(',')
-        masterName = masterInfos['hostname']
+        masterVmids = vdiGroupInfos['vmids'].split(',')
+        masterName = vdiGroupInfos['hostname']
 
         if group == "all":
             # general
-            groupInfos = getGroupInfosMaster(masterName)
-            groupInfos['actual_imagesize'] = getActualImagesize(vdiGroup)
+            groupInfos = getGroupInfosMaster(devicePath, masterName)
+            groupInfos['actual_imagesize'] = getActualImagesize(devicePath, vdiGroup)
             groupInfos['hostname'] = masterName
         else:
-            groupInfos['basic'] = getGroupInfosMaster(masterName)
-            groupInfos['basic']['actual_imagesize'] = getActualImagesize(vdiGroup)
+            groupInfos['basic'] = getGroupInfosMaster(devicePath, masterName)
+            groupInfos['basic']['actual_imagesize'] = getActualImagesize(devicePath, vdiGroup)
             groupInfos['basic']['hostname'] = masterName
 
         allApiInfos = {}
@@ -372,7 +370,7 @@ def mainMaster(group="all", quiet=False):
         for vmid in allApiInfos:
             # calculate existing
             try:
-                proxmox.nodes('hv01').qemu(vmid).status.get()
+                proxmox.nodes(node).qemu(vmid).status.get()
                 existing = existing + 1
                 # calculate buildstates - just from existing vms!
                 try:
@@ -398,10 +396,12 @@ def mainMaster(group="all", quiet=False):
             allGroupInfos[vdiGroup] = groupInfos
 
     if group == "all":
-        dbprint(json.dumps(allGroupInfos, indent=2))
+        if not quiet:
+            dbprint(json.dumps(allGroupInfos, indent=2))
         return allGroupInfos
     else:
-        dbprint(json.dumps(groupInfos, indent=2))
+        if not quiet:
+            dbprint(json.dumps(groupInfos, indent=2))
         return groupInfos
 
 
@@ -411,22 +411,21 @@ if __name__ == "__main__":
     for x in range(len(sys.argv)):
         if sys.argv[x] == "-quiet":
             quiet = True
-            
+
     if sys.argv[1] == "-master":
         if quiet == True:
-                mainMaster(quiet=True)
-            else:
-                mainMaster()
-        mainMaster()
+            mainMaster(quiet=True)
+        else:
+            mainMaster()
     elif sys.argv[1] == "-clones":
         if quiet == True:
-                mainClones(quiet=True)
-            else:
-                mainClones()
+            mainClones(quiet=True)
+        else:
+            mainClones()
     else:
         group = sys.argv[1]
         if sys.argv[2] == "-master":
-             if quiet == True:
+            if quiet == True:
                 mainMaster(group, quiet=True)
             else:
                 mainMaster(group)
@@ -434,6 +433,6 @@ if __name__ == "__main__":
             if quiet == True:
                 mainClones(group, quiet=True)
             else:
-               mainClones(group)
+                mainClones(group)
         else:
             print("***** wrong parameter! *****")

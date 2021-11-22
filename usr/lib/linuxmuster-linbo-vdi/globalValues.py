d9ea44c325f7771eabd37dee066482931b6239f6
jontzon <52744821+jontzon@users.noreply.github.com>
2021-11-22 16:01:16 +0100

    Update globalValues.py
    added multischool compatibility, main improvements

--- usr/lib/linuxmuster-linbo-vdi/globalValues.py
+++ usr/lib/linuxmuster-linbo-vdi/globalValues.py
@@ -13,6 +13,7 @@ import paramiko
 import os
 import time
 import subprocess
+import configparser
 
 # returns ssh and proxmox api connection and
 # returns global values
@@ -23,8 +24,10 @@ vdiConfig = json.loads(vdiConfigStr)
 
 global node
 node = vdiConfig['node']
-global pool
-pool = vdiConfig['pool']
+#global pool
+#pool = vdiConfig['pool']
+global mutlischool
+multischool = vdiConfig['multischool']
 global hvIp
 hvIp = vdiConfig['hvIp']
 global hvUser
@@ -37,6 +40,8 @@ global vdiLocalService # True => running service on server VM,# False => remote
 vdiLocalService = vdiConfig['vdiLocalService']
 global debugging
 debugging = vdiConfig['debugging']
+global nmapPorts
+nmapPorts = vdiConfig['nmapPorts'].split(',')
 
 # set debugging options
 def dbprint(println):
@@ -51,8 +56,6 @@ if vdiLocalService == False:
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(serverIp, port=22, username='root', password=password)
-#else:
-    #ssh = "Plätzchen"
 
 global proxmox
 proxmox = ProxmoxAPI(hvIp, user=hvUser, password=password, verify_ssl=False )
@@ -70,9 +73,7 @@ def getFileContent(pathToFile):
 # get command output from shell remote or local
 def getCommandOutput(command):
     if vdiLocalService == True:
-        #output = subprocess.check_output(command)
         output = subprocess.Popen(command,stdout=subprocess.PIPE,shell=True)
-        #output = process.communicate()[0]
         output = output.stdout.readlines()
         return output
     elif vdiLocalService == False: # returns ssh_stdout! not useable for ssh_stderr
@@ -90,11 +91,22 @@ def setCommand(command):
 # general Functions:
 def getMasterDetails(vdiGroup):
     try:
-        remotePath = "/srv/linbo/start.conf." + str(vdiGroup) + ".vdi"
-        output = getFileContent(remotePath)
+        startConf = "/srv/linbo/start.conf." + str(vdiGroup) + ".vdi"
+        output = getFileContent(startConf)
         data = json.load(output)
         return data
     except Exception as err:
+        print(err)
+
+def getSchoolId(vdiGroup):
+    try:
+        parser = configparser.ConfigParser(strict=False)
+        linboGroupConfigPath = "/srv/linbo/start.conf." + str(vdiGroup)
+        parser.read(linboGroupConfigPath)
+        schoolId = parser['LINBO']['School']      
+        return schoolId
+    except Error as err:
+        print("Problem finding school ID")
         print(err)
 
 # check server connection
@@ -135,19 +147,60 @@ def checkConnections():
 # get all vdi groups
 def getVDIGroups():
     command = "ls /srv/linbo/*.vdi"
-    #ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
     files = getCommandOutput(command)
-    #files = ssh_stdout.readlines()
     vdiGroups = []
-    for file in files:
-        file = str(file, 'ascii') # bei local Subprocess Abfrage wird Byte Object returned, bei ssh testen!
-        #print(file)
-        if file != "":
-            file = file.strip()
-            file = file.rstrip('\\n')
-            file = file.lstrip('/srv/linbo/start.conf.')
-            file = file.rstrip('.vdi')
-            vdiGroups.append(file)
-    #dbprint("***** Groups: *****")
-    #dbprint ("***** " + str(vdiGroups) + " *****")
+    for filex in files:
+        line = str(filex, 'ascii') # bei local Subprocess Abfrage wird Byte Object returned, bei ssh testen!
+        if line != "":
+            line = line.strip()
+            line = line.rstrip('\\n')
+            if line.startswith('/srv/linbo/start.conf.'):
+                line = line[len('/srv/linbo/start.conf.'):]
+            line = line.rstrip('.vdi')
+            vdiGroups.append(line)
     return vdiGroups
+
+
+###### returns dict{} logedIn{user : { ip : ip, domain : domain}
+def getSmbstatus(schoolId = "default-school"):
+    commandSmbstatus = "smbstatus | grep users"
+    if schoolId == "default-school":
+            smbstatus = getCommandOutput(commandSmbstatus)
+            logedIn = {}
+            for line in smbstatus:
+                line = str(line, 'ascii')
+                ip = line.split()[3]
+                user = line.split()[1]
+                domain, user = user.split("\\")
+                if line.split()[2] == "users":
+                    logedIn[user]= {"ip": ip, "domain": domain, "full": r"{}\{}".format(domain,user)}
+            return logedIn
+    else:
+            commandNetConfList = "net conf list"
+            netconflist = getCommandOutput(commandNetConfList)
+            try: 
+                for line in netconflist:
+                    line = str(line, 'ascii')
+                    if  schoolId in line and "msdfs proxy" in line:
+                        fileserver = (line.split(' ')[3])
+                        suffix = "/" + schoolId + "\n"
+                        fileserver = fileserver.rstrip(str(suffix))  
+                        fileserver = fileserver.lstrip("//")
+                        sshSmbstatus = paramiko.SSHClient()
+                        sshSmbstatus.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                        sshSmbstatus.connect(fileserver, port=22, username='root', password=password)
+                        sshSmbstatus_stdin, sshSmbstatus_stdout, sshSmbstatus_stderr = sshSmbstatus.exec_command(commandSmbstatus)      
+                        
+                        logedIn = {}
+                        for line in sshSmbstatus_stdout.readlines():
+                            #line = str(line, 'ascii')
+                            ip = line.split()[4]
+                            user = line.split()[1]
+                            domain, user = user.split("\\")
+                            if line.split()[3] == "users":
+                                logedIn[user]= {"ip": ip, "domain": domain, "full": r"{}\{}".format(domain,user)}
+                        return logedIn
+            except Exception as err:
+                print("Some Error while net conf list to fileserver")
+                print(err)
+                
